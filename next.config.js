9a7de93525789cf389c0e260cc3a942f0ea806fc
Ali Gunes <agunes372@gmail.com>
2025-02-26 00:50:33 +0300

    Fix the CORS

--- next.config.js
+++ next.config.js
@@ -3,6 +3,27 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*'  // Or specify your domain like 'https://saydamemek.com'
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT'
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+          }
+        ]
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig 
